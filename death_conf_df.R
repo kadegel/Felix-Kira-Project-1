70e31228aa9bc00f03225f42a05e2459dabcba01
FEL1HIL <flopez8378@student.palomar.edu>
2025-04-01 19:06:25 -0700

    Updated death_conf_df.R and added deaths_analysis files

--- death_conf_df.R
+++
@@ -1,3 +0,0 @@
-# Create data frames for deaths and confirmations from csv files
-deaths_df <- readr::read_csv("time_series_covid19_deaths_global.csv")
-confirmations_df <- readr::read_csv("time_series_covid19_confirmed_global.csv")
